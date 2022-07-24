c92de5857ea1b50b7f2465628ae1c866806ac971
1ilsang <1ilsang@naver.com>
2022-07-24 16:39:34 +0900

    feat: Set useWindowSize

--- apps/front/pages/_app.tsx
+++ apps/front/pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
+import useWindowSize from "../hooks/useWindowSize";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [width, height] = useWindowSize();
+
+  return width < 1240 ? (
+    <>
+      <h1>1240px 이하는 지원하지 않습니다.</h1>
+      <h3>
+        width: {width}, height: {height}
+      </h3>
+    </>
+  ) : (
+    <Component {...pageProps} />
+  );
 }
 
 export default MyApp;
