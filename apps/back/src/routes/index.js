56ec313782d9494b62222fd839cac180ef43a224
DevGun <devgun@kakao.com>
2022-07-27 04:25:15 +0900

    feat: /user/all test api

--- apps/back/src/routes/index.js
+++ apps/back/src/routes/index.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
-const router = Router()
+const router = Router();
 const board = require("./board");
+const user = require("./user");
 
+// 유저
+router.get("/user/all", user.show_all); // 모든 회원정보 보기
+
+// Article - 게시글 == 채팅방
 router.get("/api/allarticles", board.getAllArticles);
 
 module.exports = router;
---
+++ apps/back/src/routes/user.js
@@ -0,0 +1,6 @@
+module.exports = {
+  show_all: async (_, res) => {
+    console.log("우얏호");
+    res.send(200);
+  },
+};
