a323327592857722983a6cebb0cd22ae424d769c
1ilsang <1ilsang@naver.com>
2022-07-28 20:20:04 +0900

    feat(server): Add randomUUID

--- apps/back/src/app.js
+++ apps/back/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const { randomUUID } = require("crypto");
 
 const http = require("http");
 const server = http.createServer(app);
