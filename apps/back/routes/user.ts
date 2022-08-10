7a77dbfb4fa1499ff2cc226c149b16e7c9ef8a8b
DevGun <devgun@kakao.com>
2022-08-10 20:48:08 +0900

    docs: signup Swagger

--- apps/back/routes/user.ts
+++ apps/back/routes/user.ts
@@ -1,19 +1,11 @@
 import { Request } from "express";
-import { Schema, model, connect } from "mongoose";
+import { Schema, model } from "mongoose";
 import { UserInfo } from "opm-models";
+// const MUUID = require('uuid-mongodb');
 
 // 상태코드 정의
-const ALREADY_ID: string = "존재하는 ID 입니다.";
-
-// interface UserInfo {
-//   uId: string;
-//   uCreateDate: string;
-//   uEmail: string;
-//   uFirstName: string;
-//   uLastName: string;
-//   uNickName: string;
-//   uPassword: string;
-// }
+const ALREADY_ID: string = "존재하는 ID 입니다";
+const CREATED_ID: string = "회원가입 완료";
 
 // Mongoose
 const userSchema = new Schema<UserInfo>({
@@ -29,20 +21,6 @@ const userSchema = new Schema<UserInfo>({
 userSchema.set("collection", "User");
 const User = model<UserInfo>("User", userSchema);
 
-// 중복 이메일 확인
-async function checkEmail(email) {
-  {
-    console.log("checkEmail Func.", email);
-    const targetEmail = await User.find({ uEmail: email });
-    console.log("중복 확인 메일: ", targetEmail);
-    if (targetEmail.length === 0) {
-      return { data: "해당하는 유저가 없습니다. ", result: false };
-    } else {
-      return { data: "해당하는 유저가 있습니다.", result: true };
-    }
-  }
-}
-
 const showAllUser = async (req, res) => {
   const allUser = await User.find();
   return res.json(allUser);
@@ -51,24 +29,24 @@ const showAllUser = async (req, res) => {
 const signUpUser = async (req: Request, res) => {
   console.log("Requests:", req.body);
   console.log("Request email:", req.body.uEmail);
-  // const body = JSON.parse(req.body);
-  // var checking = await checkEmail(req.body.uEmail);
-  // console.log("User check: ", checking.result);
   const newUser = new User({
     uEmail: req.body.uEmail,
     uPassword: req.body.uPassword,
+    uFirstName: req.body.uFirstName,
+    uLastName: req.body.uLastName,
   });
+  // 중복 이메일 확인
   const checkUser = await User.find({ uEmail: req.body.uEmail });
   console.log(checkUser.length);
   if (checkUser.length) {
-    console.log("하위");
+    console.log("이미 존재하는 사용자입니다.");
+    return res.send(ALREADY_ID);
   } else {
-    console.log("바위");
+    console.log("존재하지 않는 사용자입니다.");
+    console.log("회원등록 정보:", newUser);
+    await newUser.save();
+    return res.send(CREATED_ID);
   }
-  await newUser.save({});
-  console.log(newUser);
-
-  return res.send(ALREADY_ID);
 };
 
 const user = {
