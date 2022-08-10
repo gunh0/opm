4fd31e9c0eebeaeaddac6e9a439252021b14fe21
DevGun <devgun@kakao.com>
2022-08-10 19:25:33 +0900

    fix: user interface load

--- apps/back/routes/user.ts
+++ apps/back/routes/user.ts
@@ -1,18 +1,37 @@
 import { Request } from "express";
-import mongoose from "mongoose";
-import { UserInfo } from "opm-models";
+import { Schema, model, connect } from "mongoose";
+// import { UserInfo } from "opm-models";
 
 // 상태코드 정의
 const ALREADY_ID: string = "존재하는 ID 입니다.";
 
+interface UserInfo {
+  uId: string;
+  uCreateDate: string;
+  uEmail: string;
+  uFirstName: string;
+  uLastName: string;
+  uNickName: string;
+  uPassword: string;
+}
+
 // Mongoose
-const userModel = new mongoose.Schema<UserInfo>([] as unknown as UserInfo);
-userModel.set("collection", "User");
-const User = mongoose.model("User", userModel);
+const userSchema = new Schema<UserInfo>({
+  uId: String,
+  uCreateDate: String,
+  uEmail: String,
+  uFirstName: String,
+  uLastName: String,
+  uNickName: String,
+  uPassword: String,
+});
+userSchema.set("collection", "User");
+const User = model<UserInfo>("User", userSchema);
 
 // 중복 이메일 확인
 async function checkEmail(email) {
   {
+    console.log("checkEmail Func.", email);
     const targetEmail = await User.find({ uEmail: email });
     console.log("중복 확인 메일: ", targetEmail);
     if (targetEmail.length === 0) {
@@ -23,23 +42,29 @@ async function checkEmail(email) {
   }
 }
 
-// const newUser = new User<UserInfo>({
-//   uId: "bablabmlakmsldkfm",
+// export interface UserInfo {
+//   uId: string;
+//   uCreateDate: string;
+//   uEmail: string;
+//   uFirstName: string;
+//   uLastName: string;
+//   uNickName: string;
+//   uPassword: string;
+//   uStatus: USER_STATUS;
+// }
+
+// const newUser = new User({
+//   uId: "asdfasdfsad",
 //   uCreateDate: "datetime",
 //   uEmail: "@",
-//   uName: "test",
+//   uFirstName: "test",
+//   uLastName: "test",
 //   uNickName: "testtest",
 //   uPassword: "password",
 //   uStatus: "ONLINE",
 // });
-
-// newUser.save(function (error, data) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(newUser, "Saved!");
-//   }
-// });
+// console.log("=======", newUser.uId);
+// await newUser.save();
 
 const showAllUser = async (req, res) => {
   const allUser = await User.find();
@@ -47,10 +72,18 @@ const showAllUser = async (req, res) => {
 };
 
 const signUpUser = async (req: Request, res) => {
-  console.log("Request email:", req.body);
+  console.log("Requests:", req.body);
+  console.log("Request email:", req.body.uEmail);
   // const body = JSON.parse(req.body);
   // var checking = await checkEmail(req.body.uEmail);
   // console.log("User check: ", checking.result);
+  const newUser = new User({
+    uEmail: req.body.uEmail,
+    uPassword: req.body.uPassword,
+  });
+  await newUser.save();
+  console.log(newUser);
+
   return res.send(ALREADY_ID);
 };
 
