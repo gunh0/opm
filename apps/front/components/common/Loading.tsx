8dd043d5e1019271beda91833e6ef43e6a719382
1ilsang <1ilsang@naver.com>
2022-08-18 20:41:03 +0900

    feat(board): Set loading

--- apps/front/components/common/Loading.tsx
+++ apps/front/components/common/Loading.tsx
@@ -5,15 +5,19 @@ interface LoadingProps {
   width?: string;
   height?: string;
   position?: NonNullable<JSX.IntrinsicElements["div"]["style"]>["position"];
+  top?: NonNullable<JSX.IntrinsicElements["div"]["style"]>["top"];
+  left?: NonNullable<JSX.IntrinsicElements["div"]["style"]>["left"];
 }
 
 const Loading: FunctionComponent<LoadingProps> = ({
   width = "300px",
   height = "300px",
   position = "relative",
+  top = "auto",
+  left = "auto",
 }) => {
   return (
-    <div style={{ width, height, position }}>
+    <div style={{ width, height, position, top, left }}>
       <Image src="/loading.gif" layout="fill" alt="loading" />
     </div>
   );
