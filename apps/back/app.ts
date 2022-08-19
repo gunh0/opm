e21d5f976b77a41b9a7d97ccf60ea2e479161cd4
gunh0 <devgun@kakao.com>
2022-08-19 20:49:05 +0900

    feat: cross env, localhost & ncp server

--- apps/back/app.ts
+++ apps/back/app.ts
@@ -1,4 +1,6 @@
 import http from "http";
+import https from "https";
+import fs from "fs";
 
 import cors from "cors";
 import express from "express";
@@ -14,19 +16,39 @@ import { runSocket } from "./socket/event";
 
 dotEnv.config();
 const app = express();
-const opmServer = http.createServer(app);
+
+let opmServer;
+const liveServer = "/etc/letsencrypt/live/proofor.cf/cert.pem";
 const PORT = 8080;
+const liveServerChecker = () => {
+  if (fs.existsSync(liveServer)) {
+    const options = {
+      key: fs.readFileSync("/etc/letsencrypt/live/proofor.cf/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/proofor.cf/cert.pem"),
+    };
+    opmServer = https.createServer(options, app);
+    console.info("proofor live server.");
+    opmServer.listen(PORT, () => {
+      console.info(`Listening on port ${PORT}`);
+      const io = new Server(opmServer, { cors: { origin: "*" } });
+      runSocket(io);
+    });
+  } else {
+    opmServer = http.createServer(app);
+    console.info("localhost");
+    opmServer.listen(PORT, () => {
+      console.info(`Listening on port ${PORT}`);
+      const io = new Server(opmServer, { cors: { origin: "*" } });
+      runSocket(io);
+    });
+  }
+};
+liveServerChecker();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(routes);
-
-opmServer.listen(PORT, () => {
-  console.info(`Listening on port ${PORT}`);
-  const io = new Server(opmServer, { cors: { origin: "*" } });
-  runSocket(io);
-});
 
 const mongooseOption: ConnectOptions = {
   dbName: "OPM",
